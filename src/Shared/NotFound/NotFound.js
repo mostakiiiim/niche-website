386b253cde8a4b27813f95b09e32336ca91c3f0c
mostakiiiim <mostakiiiim@gmail.com>
2021-11-13 22:57:32 +0600

    site name updatedd

--- src/Shared/NotFound/NotFound.js
+++ src/Shared/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div>
-            <h1>Not Found</h1>
+            <div>
+                <img className="img-fluid" src="https://image.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg" alt="" />
+                <Link to="/">
+                    <button className="btn btn-dark">
+                        Go Back
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 };
