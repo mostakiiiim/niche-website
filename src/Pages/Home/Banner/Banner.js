deadab5f770107f80024a69b9c2d73aef7826137
mostakiiiim <mostakiiiim@gmail.com>
2021-11-26 20:34:02 +0600

    added try

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
         <div className="banner-content ">
             <div className="row">
                 <div className="col-md-6 col-12 mt-5 d-flex justify-content-center align-contents-center align-items-center ">
-                    <h1> Let's Explore  <br /> <span className="fw-bold">A BROAD RANGE OF TRENDY EYEWEAR</span> <br />
+                    <h1> Let's Try  <br /> <span className="fw-bold">A BROAD RANGE OF TRENDY EYEWEAR</span> <br />
                         <Nav.Link as={Link} to="/allProducts">
                             <button className="btn btn-danger text-white mt-5 ">SHOP NOW</button>
                         </Nav.Link>
